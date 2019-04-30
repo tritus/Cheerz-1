1a15faabeb8357a15e260e14a4fad445d22a296b
jeremie colin <jeremie.colin@happn.fr>
2019-05-01 01:11:10 +0200

    Create retrofit dependency

---
+++ app/src/main/java/com/github/colinjeremie/cheerz/framework/NetworkPictureSource.kt
@@ -0,0 +1,31 @@
+package com.github.colinjeremie.cheerz.framework
+
+import com.github.colinjeremie.cheerz.framework.api.Api
+import com.github.colinjeremie.data.PicturesSource
+import com.github.colinjeremie.domain.Picture
+import com.google.gson.Gson
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+class NetworkPictureSource(private val gson: Gson) : PicturesSource {
+
+    private val api: Api by lazy {
+        val okHttpClient = OkHttpClient().newBuilder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.nasa.gov")
+            .client(okHttpClient)
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        retrofit.create(Api::class.java)
+    }
+
+    override fun getPicturesSinceDate(date: Date): List<Picture> = api.getPictures(gson.toJson(date)).await()
+}
---
+++ app/src/main/java/com/github/colinjeremie/cheerz/framework/api/Api.kt
@@ -0,0 +1,12 @@
+package com.github.colinjeremie.cheerz.framework.api
+
+import com.github.colinjeremie.domain.Picture
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+
+    @GET("planetary/apod")
+    fun getPictures(@Query("date") date: String): Deferred<List<Picture>>
+}
